84868e92996cd03adc7dcd253b3a83944a430c80
arvindvdeshmukh <62872067+arvindvdeshmukh@users.noreply.github.com>
2020-03-30 16:12:37 +0530

    Add files via upload

---
+++ Default.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    public string GetIP()
+    {
+        string Str = "";
+        Str = System.Net.Dns.GetHostName();
+        IPHostEntry ipEntry = System.Net.Dns.GetHostEntry(Str);
+        IPAddress[] addr = ipEntry.AddressList;
+        return addr[addr.Length - 1].ToString();
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        lblip.Visible = true;
+        lblip.Text = GetIP().ToString();
+    }
+}
